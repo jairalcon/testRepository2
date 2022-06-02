f0a42ea41f0a761ae4de2d719ec2b613a25cd7e6
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-06-02 10:58:58 -0500

    adding more test folders and files

---
+++ test2/index.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 12;
+let z = 120
+
+console.log("The product of x and y = " + (x * y) +"." );
+
+console.log(x == y);
+console.log("Boolean example is as follows: " + ((x * y == z) + "."));
